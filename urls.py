948b088b3aa915e74f0d1901f4a8a7dcc244f039
io <stuart@kieser.co.za>
2023-09-14 19:58:34 +0200

    Bulk Update
    Bulk update series of products

---
+++ urls.py
@@ -0,0 +1,3 @@
+seeff = "https://www.seeff.com/results/residential/for-sale/midrand/all/?advanced_search=1ac10ec7dee9ff3422d601283641d352"
+pp = "https://www.privateproperty.co.za/for-sale/gauteng/midrand/24"
+p24 = "https://www.property24.com/houses-for-sale/midrand/gauteng/16/"
